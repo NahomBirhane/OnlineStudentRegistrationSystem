4316176c6583661a2c92370d3ec2bb3ffc8a582b
nahom <nbirhane@gmail.com>
2024-08-07 23:20:10 -0500

    added student app

---
+++ src/main/resources/registration.sql
@@ -0,0 +1,10 @@
+INSERT INTO students (student_number, first_name, middle_name, last_name, cgpa, enrollment_date, is_international)
+VALUES
+    ('S001', 'Nahom', 'A.', 'Birhane', 3.8, '2020-09-01', 'YES'),
+    ('S002', 'John', 'B.', 'Doe', 3.5, '2019-09-01', 'NO'),
+    ('S003', 'Jane', 'C.', 'Smith', 3.9, '2021-01-15', 'YES'),
+    ('S004', 'Alice', 'D.', 'Johnson', 3.7, '2020-09-01', 'NO'),
+    ('S005', 'Michael', 'E.', 'Brown', 3.6, '2019-09-01', 'NO'),
+    ('S006', 'Mengis', 'F.', 'Clark', 3.4, '2021-01-15', 'YES'),
+    ('S007', 'Jossie', 'G.', 'Davis', 3.5, '2020-09-01', 'NO'),
+    ('S008', 'Mussie', 'H.', 'Evans', 3.8, '2019-09-01', 'YES');
